f3db5b49d53b7eaea581b3a9461f732ef632220b
Tatia Gujabidze <tatiagujabidze@gmail.com>
2024-09-08 21:46:27 -0400

    My comments are added

--- 2.7/2.7PageObject.ts
+++ 2.7/2.7PageObject.ts
@@ -1,6 +1,7 @@
 import { BasePage } from "./basePage";
 import {By} from 'selenium-webdriver'; 
 
+//Fill out the locators set in the class. ‘Hint new employee needs to be text’.
 export class pageObject extends BasePage {
     addEm: By = By.name('addEmployee');
     newEm: By = By.xpath('//li[text()="New Employee"]');
